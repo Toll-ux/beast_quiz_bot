3aaf10f046ef661ef166fb1f141e40f083a2abe0
Turtle <amuhacev083@gmail.com>
2026-09-26 18:07:56 +0500

    класс Question теперь record класс

--- src/domain/Question.java
+++ src/domain/Question.java
@@ -2,12 +2,10 @@ package domain;
 
 import java.util.List;
 
-public class Question {
-    private final int id;
-    private final String text;
-    private final List<String> answers;
-    private final int correctAnswerIndex; // номер в списке
-
+/**
+ * @param correctAnswerIndex номер в списке
+ */
+public record Question(int id, String text, List<String> answers, int correctAnswerIndex) {
     public Question(int id, String text, List<String> answers, int correctAnswerIndex) {
         this.id = id;
         this.text = text;
@@ -15,24 +13,13 @@ public class Question {
         this.correctAnswerIndex = correctAnswerIndex;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public List<String> getAnswers() {
+    @Override
+    public List<String> answers() {
         return List.copyOf(answers);
     }
 
-    public int getCorrectAnswerIndex() {
-        return correctAnswerIndex;
-    }
-
-    public boolean isCorrectAnswer(int ans){
-        if(ans < 0 || ans >= answers.size()){
+    public boolean isCorrectAnswer(int ans) {
+        if (ans < 0 || ans >= answers.size()) {
             throw new IllegalArgumentException("Answer index is out of range");
         }
 
